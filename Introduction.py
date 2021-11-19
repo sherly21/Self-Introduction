436f802ea27b01491284940b5c4900be5932963a
Sherly <89398628+sherly21@users.noreply.github.com>
2021-11-19 15:16:49 +0700

    Add files via upload

---
+++ Introduction.py
@@ -0,0 +1,8 @@
+nama = "Sherly Dwi Cecilia"
+nama_pendek = "Sherly"
+prodi = "Teknologi Informasi "
+univ = "Universitas Jember"
+NIM = "2061"
+kel = "6"
+
+print("Haloo", "Namaku "+ nama, "biasa dipanggil " + nama_pendek, "dari prodi " + prodi + univ, "Dengan NIM " + NIM, "dari Kelompok " + kel)
